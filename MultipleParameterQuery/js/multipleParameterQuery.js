b4ac54629c4f382ba818ae2d4101b4e3403ea7ff
David Muthami <waruid@gmail.com>
2014-09-07 20:07:22 +0300

    Modifications to the MultipleParameterQuery.js

--- MultipleParameterQuery/js/multipleParameterQuery.js
+++ MultipleParameterQuery/js/multipleParameterQuery.js
@@ -1,5 +1,6 @@
-﻿
-var app = {}; //Global variable object store
+﻿//Global variable object store
+var app = {}; 
+//Othe varibales in the global variable store
 app.map = null, app.toolbar = null, app.tool = null; app.symbols = null, app.printer = null;
 
 require([
@@ -31,6 +32,7 @@ require([
     //url
     var url = "http://localhost:6080/arcgis/rest/services/Otjiwarongo/MapServer";
 
+    //Search URL that is only querying the parcels feature class in the parcels fabric dataset
     var searchURL = url + "/18"
 
     //-------------------End of Variables initialization----------------------------------------------
@@ -68,10 +70,11 @@ require([
     function searchMultipleErf(erven, local_Authority_Prefix, searchURL) {
         //build query task
         var queryTask = new QueryTask(searchURL);
-
         //build query filter
         var query = new Query();
+        //Return geomeries as part of the query results
         query.returnGeometry = true;
+        //Specify outfields
         query.outFields = [
                             local_Authority_Prefix + "_local_authority_id",
                             local_Authority_Prefix + "_stand_no",
@@ -88,50 +91,58 @@ require([
         ];
         //Build the where clause
         var myWhere = "";
-        //loop through the array of provided erven
-        for (index = 0; index < erven.length; index++) {
-            //for the last array ellement
+
+        //loop through the array of provided erven . below code has been thrown out of the window
             /*
-             *
+                    for (index = 0; index < erven.length; index++) {
+                        //for the last array ellement   
+                         *
+                        if (index == erven.length - 1) {
 
-            if (index == erven.length - 1) {
+                            myWhere += local_Authority_Prefix + "_erf_no = '" + erven[index] + "'"; //minus or
 
-                myWhere += local_Authority_Prefix + "_erf_no = '" + erven[index] + "'"; //minus or
+                        } else { //Other array elements
 
-            } else { //Other array elements
+                            myWhere += local_Authority_Prefix + "_erf_no = '" + erven[index] + "' or "; // with or
+                        }
+            
 
-                myWhere += local_Authority_Prefix + "_erf_no = '" + erven[index] + "' or "; // with or
-            }
+                    }
              */
 
-        }
-
+        /*
+         * Having one in clause over multiple or clauses
+         * Check the erven has search erfs
+         */
         if (erven.length > 0) {
             var myWhere = local_Authority_Prefix + "_erf_no in ('" + erven.join("','") + "')";
-            console.log(myWhere);
+            //console.log(myWhere);
         }
-
-        //console.log(myWhere);
 
         //Where clause
         query.where = myWhere;
 
         //Spatial Reference
         query.outSpatialReference = { "wkid": 102100 };//web mercator auxiliary sphere
-
-        //Pop-up template
-        var infoTemplate = new InfoTemplate();
-        infoTemplate.setTitle("${oj_stand_no}");
-        infoTemplate.setContent("<b>Township: </b>${oj_township_id}<br/>"
-                             + "<b>Erf No </b>${oj_erf_no}<br/>"
-                             + "<b>Zoning: </b>${oj_zoning_id}");
-
+        //Pop-up template for the graphics added onto the map upon successful query execution
+        var infoTemplate = new InfoTemplate();//instantiate  pop-up object
+        //Set-up pop-up tital
+        infoTemplate.setTitle("${" + local_Authority_Prefix + "_stand_no}");
+        //set-up content to be displayed on te pop-up
+        infoTemplate.setContent("<b>Township: </b>${" + local_Authority_Prefix + "_township_id}<br/>"
+                             + "<b>Erf No </b>${" + local_Authority_Prefix + "_erf_no}<br/>"
+                             + "<b>Zoning: </b>${" + local_Authority_Prefix + "_zoning_id}");
+        //Set-up info window width and height
         app.map.infoWindow.resize(245, 105);
 
         //Can listen for onComplete event to process results or can use the callback option in the queryTask.execute method.
         dojo.connect(queryTask, "onComplete", function (featureSet) {
+            //Clear any existing graphic ayers on the map
             app.map.graphics.clear();
-
+            /*
+             * Create symbol object
+             * Create fill symbol and outline symbol and apply colour
+             */
             var symbol = new esri.symbol.SimpleFillSymbol(
                 esri.symbol.SimpleFillSymbol.STYLE_SOLID,
                 new esri.symbol.SimpleLineSymbol(
@@ -146,13 +157,13 @@ require([
             dojo.forEach(featureSet.features, function (feature) {
                 var graphic = feature;
                 graphic.setSymbol(symbol);
-
                 //Set infotemplate for graphic layer only
                 graphic.setInfoTemplate(infoTemplate);
-
+                //add graphic to the map
                 app.map.graphics.add(graphic);
             });
         });
+        //execute asynchronous query task by passing query parameter to ts execute method
         queryTask.execute(query);
     }
 
